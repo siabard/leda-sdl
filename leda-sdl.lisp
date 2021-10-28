ad8ce08c7a65c787330c9a88c6b0efb8f9c175e9
Yeonho Jang <siabard@gmail.com>
2021-10-28 22:55:03 +0900

    Show Image

--- leda-sdl.lisp
+++ leda-sdl.lisp
@@ -2,6 +2,12 @@
 
 (in-package #:leda-sdl)
 
+
+(defun clear-renderer (renderer)
+  (progn
+    (sdl2:set-render-draw-color renderer 0 0 0 255)
+    (sdl2:render-clear renderer)))
+
 (defun basic-main ()
   (sdl2:with-init (:everything)
     (format t "Using SDL Library Version: ~D. ~D. ~D~%"
@@ -12,9 +18,19 @@
 
     (sdl2:with-window (win :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
-	(sdl2:with-event-loop (:method :poll)
-	  (:keydown (:keysym keysym)
-		    (let ((scancode (sdl2:scancode-value keysym)))
-		      (cond
-			((sdl2:scancode= scancode :scancode-escape) (sdl2:push-event :quit)))))
-	  (:quit () t))))))
+	(sdl2:with-renderer (renderer win :flags '(:accelerated))
+	  (progn
+	    (sdl2-image:init '(:png :jpg))
+	    (let* ((img-surface (sdl2-image:load-image (asdf:system-relative-pathname :leda-sdl "assets/grass_tile.png")))
+		   (img-texture (sdl2:create-texture-from-surface renderer img-surface )))
+	      (sdl2:with-event-loop (:method :poll)
+		(:keydown (:keysym keysym)
+			  (let ((scancode (sdl2:scancode-value keysym)))
+			    (cond
+			      ((sdl2:scancode= scancode :scancode-escape) (sdl2:push-event :quit)))))
+		(:idle ()
+		       (clear-renderer renderer)
+		       (sdl2:render-copy renderer img-texture)
+		       (sdl2:render-present renderer))
+		(:quit () t)))))))
+    ))
